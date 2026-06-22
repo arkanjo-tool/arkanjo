5f401b86a3f0147c8ac25f608e9bc17b1fcf165a
mole28 <80823255+mole28@users.noreply.github.com>
2026-06-23 00:17:46 +0300

    Refactor error handling in PreprocessorClean
    Replace std::cerr with CLIError for error handling.

--- src/commands/pre/clean/preprocessor_clean.cpp
+++ src/commands/pre/clean/preprocessor_clean.cpp
@@ -1,12 +1,13 @@
 #include "preprocessor_clean.hpp"
 #include <arkanjo/base/config.hpp>
+#include <arkanjo/cli/cli_error.hpp>
 #include <filesystem>
 #include <iostream>
+#include <string> 
 
 namespace fs = std::filesystem;
 
 bool PreprocessorClean::validate([[maybe_unused]] const ParsedOptions& options) {
-
     return true;
 }
 
@@ -15,14 +16,13 @@ bool PreprocessorClean::run([[maybe_unused]] const ParsedOptions& options) {
         fs::path cache_dir = Config::config().base_path / Config::config().name_container;
 
         if (fs::exists(cache_dir)) {
-            fs::remove_all(cache_dir); 
+            fs::remove_all(cache_dir);
             std::cout << "Successfully cleaned cache directory: " << cache_dir.string() << "\n";
         } else {
             std::cout << "Cache directory does not exist, nothing to clean.\n";
         }
     } catch (const std::exception& e) {
-        std::cerr << "Error while cleaning cache: " << e.what() << "\n";
-        return false;
+        throw CLIError("Error while cleaning cache: " + std::string(e.what()));
     }
 
     return true;
