2da59ac3b25d4070c7cd176f07feb67527deec8b
Andre Luiz Batista Bueno <andrebatistabueno@gmail.com>
2026-06-25 20:48:37 -0300

    feat(preprocessor): implement --all option for clean command (#31)
    Add --all option in the clean command to remove all cached preprocess profiles.
    
    Co-authored-by: Enzo Furegatti Spinella <enzo.spinella@usp.br>

--- src/commands/pre/clean/preprocessor_clean.cpp
+++ src/commands/pre/clean/preprocessor_clean.cpp
@@ -8,18 +8,26 @@
 namespace fs = std::filesystem;
 
 bool PreprocessorClean::validate(const ParsedOptions& options) {
+    clean_all = options.args.count("all") > 0;
     return true;
 }
 
 bool PreprocessorClean::run(const ParsedOptions& options) {
     try {
-        fs::path cache_dir = Config::config().base_path / Config::config().name_container;
+        fs::path base_path = Config::config().base_path;
+        fs::path cache_dir = clean_all ? base_path : base_path / Config::config().name_container;
+
         if (fs::exists(cache_dir)) {
             fs::remove_all(cache_dir);
-            std::cout << "Successfully cleaned cache directory: " << cache_dir.string() << "\n";
-        } else {
-            std::cout << "Cache directory does not exist, nothing to clean.\n";
-        }
+            fs::create_directories(base_path);
+
+            if (clean_all) {
+                std::cout << "Successfully cleaned all cache directories in: " << cache_dir.string() << "\n";
+            } 
+            else std::cout << "Successfully cleaned cache directory: " << cache_dir.string() << "\n";
+        } 
+        else std::cout << "Cache directory does not exist, nothing to clean.\n";
+
     } catch (const std::exception& e) {
         throw CLIError("Error while cleaning cache: " + std::string(e.what()));
     }
